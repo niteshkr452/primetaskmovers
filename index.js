73a3fb7d6816eddf07ea2f1e231f2c059ad37a6a
NITESH YADAV <178077596+niteshkr452@users.noreply.github.com>
2025-07-21 11:31:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const path = require("path");
-require('dotenv').config();
-
-const app = express();
-
-// Middleware
-app.use(express.json());
-app.use(cors());
-
-// Serve static files from the project's root directory
-app.use(express.static(path.join(__dirname)));
-
-// MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://niteshraj355yadav:<db_password>@cluster0.rx2zd8x.mongodb.net/", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log("✅ MongoDB Connected"))
-    .catch((err) => console.error("❌ MongoDB Connection Error:", err));
-
-// Define Schema with required fields
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    subject: String,
-    message: String
-});
-
-// Create Model
-const UserModel = mongoose.model("User", userSchema);
-
-// API Endpoint to Handle Form Submission
-app.post("/api/contact", async (req, res) => {
-    try {
-        const { name, email, subject, message } = req.body;
-
-        // Create a new user instance
-        const newUser = new UserModel({
-            name,
-            email,
-            subject,
-            message
-        });
-
-        // Save the user to the database
-        await newUser.save();
-
-        res.status(201).json({ message: "✅ Your message has been sent successfully!" });
-    } catch (err) {
-        console.error("❌ Error saving data:", err);
-        res.status(500).json({ message: "❌ An error occurred while sending your message. Please try again later." });
+"use strict";
+const getCodePoint = (character) => character.codePointAt(0);
+const first = (x) => x[0];
+const last = (x) => x[x.length - 1];
+function toCodePoints(input) {
+    const codepoints = [];
+    const size = input.length;
+    for (let i = 0; i < size; i += 1) {
+        const before = input.charCodeAt(i);
+        if (before >= 0xd800 && before <= 0xdbff && size > i + 1) {
+            const next = input.charCodeAt(i + 1);
+            if (next >= 0xdc00 && next <= 0xdfff) {
+                codepoints.push((before - 0xd800) * 0x400 + next - 0xdc00 + 0x10000);
+                i += 1;
+                continue;
+            }
+        }
+        codepoints.push(before);
     }
-});
-
-// The express.static middleware will automatically serve index.html for the root path '/'
-// and also handle all other static files like about.html, contact.html, and everything in /assets.
-
-// Start Server
-const PORT = process.env.PORT || 5500;
-app.listen(PORT, () => {
-    console.log(`🚀 Server is running on http://localhost:${PORT}`);
-});
+    return codepoints;
+}
+function saslprep({ unassigned_code_points, commonly_mapped_to_nothing, non_ASCII_space_characters, prohibited_characters, bidirectional_r_al, bidirectional_l, }, input, opts = {}) {
+    const mapping2space = non_ASCII_space_characters;
+    const mapping2nothing = commonly_mapped_to_nothing;
+    if (typeof input !== 'string') {
+        throw new TypeError('Expected string.');
+    }
+    if (input.length === 0) {
+        return '';
+    }
+    const mapped_input = toCodePoints(input)
+        .map((character) => (mapping2space.get(character) ? 0x20 : character))
+        .filter((character) => !mapping2nothing.get(character));
+    const normalized_input = String.fromCodePoint
+        .apply(null, mapped_input)
+        .normalize('NFKC');
+    const normalized_map = toCodePoints(normalized_input);
+    const hasProhibited = normalized_map.some((character) => prohibited_characters.get(character));
+    if (hasProhibited) {
+        throw new Error('Prohibited character, see https://tools.ietf.org/html/rfc4013#section-2.3');
+    }
+    if (opts.allowUnassigned !== true) {
+        const hasUnassigned = normalized_map.some((character) => unassigned_code_points.get(character));
+        if (hasUnassigned) {
+            throw new Error('Unassigned code point, see https://tools.ietf.org/html/rfc4013#section-2.5');
+        }
+    }
+    const hasBidiRAL = normalized_map.some((character) => bidirectional_r_al.get(character));
+    const hasBidiL = normalized_map.some((character) => bidirectional_l.get(character));
+    if (hasBidiRAL && hasBidiL) {
+        throw new Error('String must not contain RandALCat and LCat at the same time,' +
+            ' see https://tools.ietf.org/html/rfc3454#section-6');
+    }
+    const isFirstBidiRAL = bidirectional_r_al.get(getCodePoint(first(normalized_input)));
+    const isLastBidiRAL = bidirectional_r_al.get(getCodePoint(last(normalized_input)));
+    if (hasBidiRAL && !(isFirstBidiRAL && isLastBidiRAL)) {
+        throw new Error('Bidirectional RandALCat character must be the first and the last' +
+            ' character of the string, see https://tools.ietf.org/html/rfc3454#section-6');
+    }
+    return normalized_input;
+}
+saslprep.saslprep = saslprep;
+saslprep.default = saslprep;
+module.exports = saslprep;
+//# sourceMappingURL=index.js.map
